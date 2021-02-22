0417b15d0769cad57d822ef7931313c1d2571f50
Gtlions.Lai <gtlions.l@qq.com>
2021-02-22 08:23:23 +0800

    comment

--- encoding.go
+++ encoding.go
@@ -8,6 +8,8 @@ import (
 	"golang.org/x/text/transform"
 )
 
+// GbkToUtf8 字符串编码gbk->utf-8
+//
 func GbkToUtf8(s []byte) ([]byte, error) {
 	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewDecoder())
 	d, e := ioutil.ReadAll(reader)
@@ -17,6 +19,8 @@ func GbkToUtf8(s []byte) ([]byte, error) {
 	return d, nil
 }
 
+// Utf8ToGbk 字符串编码utf-8->gbk
+//
 func Utf8ToGbk(s []byte) ([]byte, error) {
 	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewEncoder())
 	d, e := ioutil.ReadAll(reader)
