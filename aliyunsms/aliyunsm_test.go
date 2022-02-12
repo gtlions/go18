1bd266a6331725d34514648fe7340f7fc6037d87
gtlions <gtlions.l@qq.com>
2022-02-12 16:10:00 +0800

    new version

--- aliyunsms/aliyunsm_test.go
+++ aliyunsms/aliyunsm_test.go
@@ -7,10 +7,10 @@ import (
 )
 
 func TestSendSms(t *testing.T) {
-	accessKeyID := "accessKeyID"
-	accessKeySecret := "accessKeySecret"
+	accessKeyID := "LTAI4G4kHXWANWNkv6xdVEfq"
+	accessKeySecret := "dYBYBMGdnjUeRdAeuWM7YCcjgVXUni"
 	client := NewClient(accessKeyID, accessKeySecret)
-	err := client.SendSms("137745536xx", "xxx", "SMS_xxxxxx", fmt.Sprintf(`{"code":%s}`, "123456"))
+	err := client.SendSms("13774553612", "芯美丽", "SMS_204756660", fmt.Sprintf(`{"code":%s}`, "123456"))
 
 	if err != nil {
 		t.Fatal("操作失败", err)
