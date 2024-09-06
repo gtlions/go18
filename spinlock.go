7cc668632f84be5175fbf000fc6f4184f313befb
Gtlions.Lai <gtlions.l@qq.com>
2024-09-06 20:35:37 +0800

    优化结构体

--- spinlock.go
+++ spinlock.go
@@ -8,14 +8,14 @@ import (
 const maxBackOff = 32
 
 type SpinLock struct {
-	L uint32
+	l uint32
 }
 
 // Lock 加锁
 func (sl *SpinLock) Lock() {
 	backoff := 1
 	// 自旋尝试获取锁
-	for !atomic.CompareAndSwapUint32((&sl.L), 0, 1) {
+	for !atomic.CompareAndSwapUint32((&sl.l), 0, 1) {
 		for i := 0; i < backoff; i++ {
 			runtime.Gosched()
 		}
@@ -27,5 +27,5 @@ func (sl *SpinLock) Lock() {
 
 // UnLock 解锁
 func (sl *SpinLock) UnLock() {
-	atomic.CompareAndSwapUint32((&sl.L), 1, 0)
+	atomic.CompareAndSwapUint32((&sl.l), 1, 0)
 }
